bfc5d0c7e2b6c873bdc3e85941a6a3b0aa02772f
aleyna çelik <celikaleyna71@gmail.com>
2023-07-26 14:43:17 +0300

    Proje Finali

---
+++ CasgemUow.PresentationLayer/Controllers/DefaultController.cs
@@ -0,0 +1,43 @@
+﻿using CasgemUow.BusinessLayer.Abstract;
+using CasgemUow.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CasgemUow.PresentationLayer.Controllers
+{
+    public class DefaultController : Controller
+    {
+        private readonly ICustomerProcessService _customerProcessService;
+        private readonly ICustomerService _customerService;
+
+        public DefaultController(ICustomerProcessService customerProcessService, ICustomerService customerService)
+        {
+            _customerProcessService = customerProcessService;
+            _customerService = customerService;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(CustomerProcess customerProcess)
+        {
+            var valueSender = _customerService.TGetByID(customerProcess.SenderID);
+            var valueReciever = _customerService.TGetByID(customerProcess.ReceiverID);
+
+            valueReciever.CustomerBalance += customerProcess.Amount;
+            valueSender.CustomerBalance-=customerProcess.Amount;
+
+            List<Customer> modifiedCustomer = new List<Customer>()
+            {
+                valueSender,
+                valueReciever,
+            };
+            _customerService.TMultiUpdate(modifiedCustomer);
+            _customerProcessService.TInsert(customerProcess);
+            return RedirectToAction("CustomerProcessList");
+
+        }
+    }
+}
